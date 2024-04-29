954ed47e5f18b5f08c7faeac74823f75be67268a
Raja3561 <Raja3561@CSL0704.global.chainsys.com>
2024-04-29 17:43:05 +0530

    Collections-ArrayList,LinkedList,Queue

--- collection/src/com/chainsys/collection/ListOfArray.java
+++ collection/src/com/chainsys/collection/ListOfArray.java
@@ -26,6 +26,30 @@ public class ListOfArray {
 			System.out.println("The peek value:" + blist.peek());
 
 			System.out.println("The Clone Array" + seclist);
+			
+			Queue<Integer> q1 =new LinkedList(); 
+			
+			q.add(10);
+			q.add(2);
+			q.add(5);
+			q.add(11);
+			//queue6y
+			System.out.println("poll"+q.size());
+		System.out.println("Head is "+q.peek());
+		System.out.println("Remove"+q.remove());
+		
+	
+		PriorityQueue <Integer>q2=new PriorityQueue<Integer>();
+		
+		q2.add(9);
+		q2.add(8);
+		q2.add(1);
+		q2.add(2);
+		System.out.println(q2);
+		System.out.println("the Size:"+q2.size());
+		System.out.println("The poll: "+q2.poll());	
+	
+		
 
 		}
 
